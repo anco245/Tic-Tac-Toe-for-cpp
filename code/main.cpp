bc8838d2b2ed0a38a7b0ec6745672a415ad836c1
anco245 <98773223+anco245@users.noreply.github.com>
2022-04-26 01:40:21 -0400

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -12,6 +12,8 @@ int main() {
 
 	string position;
 	int count = 0;
+	
+	string again = "y";
 
 	board b;
 
@@ -54,91 +56,116 @@ int main() {
 		player2 = "x";
 	}
 
-	cout << "-------------------------------------" << endl;
-	cout << endl;
-
-	system("clear");
-
-	while(!b.winner())
+	//cout << "-------------------------------------" << endl;
+	//cout << endl;
+	
+	while(again=='y')
 	{
-		count++;
-
-		bool error = true;
-
-		cout << b.print() << endl;
-
-		if (count%2 == 0)
+		system("clear);
+		       
+		while(!b.winner())
 		{
-			cout << "Player 2's turn" << endl;
-			cout << "You are: " << player2 << endl;
-		} else {
-			cout << "Player 1's turn" << endl;
-			cout << "You are: " << player1 << endl;
-		}
+			count++;
 
-		cout << "Enter your answer" << endl;
-		cin >> position;
+			bool error = true;
 
-		while(error == true)
-		{
+			cout << "-------------TIK-TAC-TOE-------------" << endl;
+			cout << "|                                   |" << endl;
+			cout << "|               1 2 3               |" << endl;
+			cout << "|               4 5 6               |" << endl;
+			cout << "|               7 8 9               |" << endl;
+			cout << "|                                   |" << endl;
+			cout << "-------------------------------------" << endl;
+			cout << "|                                   |" << endl;
+			cout << "|            HOW TO PLAY:           |" << endl;
+			cout << "|   When it's your turn, pick the   |" << endl;
+			cout << "|    number that corresponds to     |" << endl;
+			cout << "|       the space you want.         |" << endl;
+			cout << "|                                   |" << endl;
+			cout << "-------------------------------------" << endl;
+
 			cout << endl;
 
-			if ((position.length() == 1) && (isdigit(position[0])) &&
-					(stoi(position) > 0) && (stoi(position) < 10))
+			cout << b.print() << endl;
+
+			if (count%2 == 0)
 			{
-				if (b.occupied(stoi(position)))
+				cout << "Player 2's turn" << endl;
+				cout << "You are: " << player2 << endl;
+			} else {
+				cout << "Player 1's turn" << endl;
+				cout << "You are: " << player1 << endl;
+			}
+
+			cout << "Enter your answer" << endl;
+			cin >> position;
+
+			while(error == true)
+			{
+				cout << endl;
+
+				if ((position.length() == 1) && (isdigit(position[0])) &&
+						(stoi(position) > 0) && (stoi(position) < 10))
 				{
-					cout << endl;
-					cout << "That space is already occupied" << endl;
+					if (b.occupied(stoi(position)))
+					{
+						cout << endl;
+						cout << "That space is already occupied" << endl;
+						cout << "Try Again" << endl;
+						cin >> position;
+
+						error = true;
+					} else {
+						error = false;
+					}
+				} else if (stoi(position) < 1 || stoi(position) > 10) {
+					error = true;
+
+					cout << "Error: Position has to be a number from 1 to 9" << endl;
 					cout << "Try Again" << endl;
 					cin >> position;
 
-					error = true;
 				} else {
-					error = false;
+					error = true;
+
+					cout << "Error: Try Again" << endl;
+					cin >> position;
 				}
-			} else if (stoi(position) < 1 || stoi(position) > 10) {
-				error = true;
-
-				cout << "Error: Position has to be a number from 1 to 9" << endl;
-				cout << "Try Again" << endl;
-				cin >> position;
-
-			} else {
-				error = true;
-
-				cout << "Error: Try Again" << endl;
-				cin >> position;
 			}
-		}
 
-		if (count%2 == 0)
-		{
-			b.answer(stoi(position), player2);
-		} else {
-			b.answer(stoi(position), player1);
+			if (count%2 == 0)
+			{
+				b.answer(stoi(position), player2);
+			} else {
+				b.answer(stoi(position), player1);
+			}
+
+			system("clear");
 		}
 
 		system("clear");
-	}
 
-	system("clear");
+		cout << b.print() << endl;
 
-	cout << b.print() << endl;
-
-	if(b.winningPlayer == player1)
-	{
-		cout << "-------------------------------------" << endl;
-		cout << "|           PLAYER 1 WINS           |" << endl;
-		cout << "-------------------------------------" << endl;
-	} else if (b.winningPlayer == player2){
-		cout << "-------------------------------------" << endl;
-		cout << "|           PLAYER 2 WINS           |" << endl;
-		cout << "-------------------------------------" << endl;
-	} else {
-		cout << "-------------------------------------" << endl;
-		cout << "|            IT'S A TIE             |" << endl;
-		cout << "-------------------------------------" << endl;
+		if(b.winningPlayer == player1)
+		{
+			cout << "-------------------------------------" << endl;
+			cout << "|           PLAYER 1 WINS           |" << endl;
+			cout << "-------------------------------------" << endl;
+		} else if (b.winningPlayer == player2){
+			cout << "-------------------------------------" << endl;
+			cout << "|           PLAYER 2 WINS           |" << endl;
+			cout << "-------------------------------------" << endl;
+		} else {
+			cout << "-------------------------------------" << endl;
+			cout << "|            IT'S A TIE             |" << endl;
+			cout << "-------------------------------------" << endl;
+		}
+		
+		cout << endl;
+		
+		cout << "Do you want to play again? (y/n)" << endl;
+		cin >> again
 	}
 
 }
